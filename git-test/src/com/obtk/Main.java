4f5ec9cc1fc0cb5e27c135dbf62faec50d6cad3e
chenjihe <1767028816@qq.com>
2020-06-19 21:25:44 +0800

    提交

---
+++ git-test/src/com/obtk/Main.java
@@ -0,0 +1,12 @@
+package com.obtk;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("11");
+        System.out.println("22");
+        System.out.println("33");
+        System.out.println("44");
+        System.out.println("55");
+    }
+}
